acd6666ebcc6c3f294d916469cffca766c57d41f
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-13 13:07:19 +0200

    feat: add basic structure

---
+++ app/controllers/TaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+class TaskController extends Controller
+{
+    public function indexAction()
+    {
+        
+    }
+
+    public function detailAction()
+    {
+        
+    }
+
+    public function createAction()
+    {
+        
+    }
+
+    public function editAction()
+    {
+        
+    }
+
+    public function deleteAction()
+    {
+        
+    }
+
+    public function completedAction()
+    {
+        
+    }
+}
