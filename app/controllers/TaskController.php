c1401d084010e732af1e1664682aa428b77fac15
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-14 13:15:09 +0200

    feat: add storeAction() method

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -17,6 +17,18 @@ class TaskController extends Controller
         
     }
 
+    public function storeAction()
+    {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+
+        $model = new TaskModel();
+        $model->addTask($title, $description, 'pending', 1);
+
+        header('Location: /');
+        exit;
+    }
+
     public function editAction()
     {
         
