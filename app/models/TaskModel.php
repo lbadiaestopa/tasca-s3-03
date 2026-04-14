42f8f0c325cd43fa5e715db63c440e689838f4f5
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-14 13:16:58 +0200

    feat: add readTasks() method

--- app/models/TaskModel.php
+++ app/models/TaskModel.php
@@ -12,6 +12,9 @@ class TaskModel
     // Canviar string per enum a $status
     public function addTask(string $title, string $description, string $status, int $userId): void
     {
+
+        $tasks = $this->readTasks();
+
         $task = [
             "id" => 1, //canviar per paràmetre
             "title" => $title,
@@ -30,8 +33,19 @@ class TaskModel
     private function writeTasks(array $tasks): void
     {
         file_put_contents(
-            $this->filePath, 
+            $this->filePath,
             json_encode($tasks, JSON_PRETTY_PRINT)
         );
+    }
+
+    private function readTasks(): array
+    {
+        if (!file_exists($this->filePath)) {
+            return [];
+        }
+
+        $content = file_get_contents($this->filePath);
+
+        return $content ? json_decode($content, true) : [];
     }
 }
