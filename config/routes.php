aee024f6bff1feeb53c22505c15e9fc4d464a75a
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-15 17:32:26 +0200

    feat: add /status and /edit/:id routes

--- config/routes.php
+++ config/routes.php
@@ -6,7 +6,9 @@ $routes = array(
     '/task/create' => 'task#create',
     '/task/delete' => 'task#delete',
     '/task/update' => 'task#update',
+    '/task/status' => 'task#status',
     '/task/edit' => 'task#edit',
+    '/task/edit/:id' => 'task#edit',
     '/task/:id' => 'task#detail',
     '/task/store' => 'task#store'
 );
