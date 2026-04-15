9be81d45486ab5f71d353850a8b3cec3cef72912
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-15 13:32:19 +0200

    feat: add task/update route

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,7 @@ $routes = array(
     '/task/completed' => 'task#completed',
     '/task/create' => 'task#create',
     '/task/delete' => 'task#delete',
+    '/task/update' => 'task#update',
     '/task/edit' => 'task#edit',
     '/task/:id' => 'task#detail',
     '/task/store' => 'task#store'
